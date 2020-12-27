27391dcd6f81d515ff46994291139385cdca51d1
Vishal.Kwatra <Vishal.Kwatra@IN2465447W4.ey.net>
2020-12-27 20:55:12 +0530

    First Commit: Understanding Rest GET and POST

---
+++ demoSpring3/src/main/java/com/ey/hr/lms/code/DemoSpring3Application.java
@@ -0,0 +1,13 @@
+package com.ey.hr.lms.code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring3Application.class, args);
+	}
+
+}
